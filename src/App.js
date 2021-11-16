1cf01c29fa6c6faebfd068c0b322efc7956fe1ee
MarcinParda <marcin98parda@gmail.com>
2021-11-16 21:51:18 +0100

    fix css, add logout button

--- src/App.js
+++ src/App.js
@@ -5,58 +5,77 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA5we1X9VqP5LYreZ8Wa9XW4YOFvGeisXc",
-  authDomain: "superchat-cc2d4.firebaseapp.com",
-  projectId: "superchat-cc2d4",
-  storageBucket: "superchat-cc2d4.appspot.com",
-  messagingSenderId: "245943375008",
-  appId: "1:245943375008:web:74e6eb60e36c1b6283e5c5"
+    apiKey: "AIzaSyA5we1X9VqP5LYreZ8Wa9XW4YOFvGeisXc",
+    authDomain: "superchat-cc2d4.firebaseapp.com",
+    projectId: "superchat-cc2d4",
+    storageBucket: "superchat-cc2d4.appspot.com",
+    messagingSenderId: "245943375008",
+    appId: "1:245943375008:web:74e6eb60e36c1b6283e5c5"
 })
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
+
 function App() {
 
-  const [user] = useAuthState(auth);
+    const [user] = useAuthState(auth);
 
-  return (
-    <div className="App">
-      <header>
+    return (
+        <div className="App">
+            <header>
+                <h1>Superchat</h1>
+                <SignOut />
+            </header>
 
-      </header>
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-    </div>
-  );
+            <section>
+                {user ? <ChatRoom /> : <SignIn />}
+            </section>
+
+        </div>
+    );
 }
 
 function SignIn() {
+
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider);
     }
 
     return (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <>
+            <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+            <p>Do not violate the community guidelines or you will be banned for life!</p>
+        </>
+    )
+
+}
+
+function SignOut() {
+    return auth.currentUser && (
+        <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
     )
 }
 
+
 function ChatRoom() {
+    const dummy = useRef();
     const messagesRef = firestore.collection('messages');
     const query = messagesRef.orderBy('createdAt').limit(25);
 
-    const [messages] = useCollectionData(query, {idField: 'id'});
-    const [formValue, setFormValue] = useState('');
-    const dummy = useRef();
+    const [messages] = useCollectionData(query, { idField: 'id' });
 
-    const sendMessage = async(e) => {
+    const [formValue, setFormValue] = useState('');
+
+
+    const sendMessage = async (e) => {
         e.preventDefault();
+
         const { uid, photoURL } = auth.currentUser;
 
         await messagesRef.add({
@@ -67,30 +86,32 @@ function ChatRoom() {
         })
 
         setFormValue('');
-        dummy.current.scrollIntoView({ behavior: 'smooth'});
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
     }
 
-    return (
-        <>
-            <div>
-                {messages && messages.map((message) =>
-                    <ChatMessage key={message.id} message={message} />
-                )}
+    return (<>
+        <main>
 
-                <div ref={dummy} />
-            </div>
-            <form onSubmit={sendMessage}>
-                <input
-                    value={formValue}
-                    onChange={(e) => setFormValue(e.target.value)}
-                    type="text"
-                    placeholder="Say hello to everybody..."
-                />
-                <button type="submit">SEND</button>
-            </form>
-        </>
-    )
+            {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+            <span ref={dummy} />
+
+        </main>
+
+        <form onSubmit={sendMessage}>
+
+            <input
+                value={formValue}
+                onChange={(e) => setFormValue(e.target.value)}
+                placeholder="send message..."
+            />
+
+            <button type="submit" disabled={!formValue}>🕊️</button>
+
+        </form>
+    </>)
 }
+
 
 function ChatMessage(props) {
     const { text, uid, photoURL } = props.message;
@@ -99,10 +120,11 @@ function ChatMessage(props) {
 
     return (
         <div className={`message ${messageClass}`}>
-            <img src={photoURL} alt="avatar"/>
+            <img src={photoURL || 'https://avatars.dicebear.com/api/adventurer/your-cust.svg'}  alt={"photo"}/>
             <p>{text}</p>
         </div>
     )
 }
+
 
 export default App;
